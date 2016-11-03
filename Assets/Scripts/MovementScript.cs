d98b2ec2f8025d8ca9bb992fcec778596cfa743a
Justin Ha <jch583@nyu.edu>
2016-11-03 15:17:01 -0400

    Moves forward

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	Rigidbody rb; 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.velocity = transform.forward * 10f;
+	}
+}
