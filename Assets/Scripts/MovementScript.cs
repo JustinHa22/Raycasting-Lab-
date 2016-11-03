8e2119cd55ee1e240e125862b8c24858308ba27e
Justin Ha <jch583@nyu.edu>
2016-11-03 15:39:52 -0400

    Can see the ray cast, must implement them into of statements now

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,10 +8,22 @@ public class MovementScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		//Ray moveray = new Ray (transform.position, transform.forward);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		rb.velocity = transform.forward * 10f;
+
+		Ray frontRay = new Ray (transform.position, transform.forward);
+		Ray rightRay = new Ray (transform.position, transform.right); 
+		Ray leftRay = new Ray (transform.position, -transform.right);
+
+		RaycastHit rayHit = new RaycastHit ();
+
+		Debug.DrawRay (frontRay.origin, frontRay.direction * 10f, Color.red);
+		Debug.DrawRay (rightRay.origin, rightRay.direction * 20f, Color.blue);
+		Debug.DrawRay (leftRay.origin, leftRay.direction * 20f, Color.yellow);
+
 	}
 }
