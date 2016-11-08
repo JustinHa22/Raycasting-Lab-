5796933ee7e8ad5def6bcd27089b92b704fad78e
Justin Ha <jch583@nyu.edu>
2016-11-07 21:14:57 -0500

    Got all scripts to work

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+
+	public Transform mouse;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		if (mouse == null) {
+			return;
+		}
+		Vector3 directionToMouse = new Vector3 ();
+		directionToMouse = mouse.position - this.transform.position;
+
+		if (Vector3.Angle (this.transform.forward, directionToMouse) < 30f) {
+			Ray catRay = new Ray (this.transform.position, directionToMouse);
+			RaycastHit catRayHitInfo;
+
+			if (Physics.Raycast (catRay, out catRayHitInfo, 100f) == true) {
+				if (catRayHitInfo.collider.tag == "Mouse" && catRayHitInfo.distance < 20f) {
+						Destroy (mouse.gameObject);
+					} 
+						this.GetComponent<Rigidbody> ().AddForce (directionToMouse.normalized * 1000f);
+					}
+				}
+			}
+}
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	//Cat catScript;
+	public Transform cat;
+
+	void Start () {
+		//catScript = GameObject.Find ("Cat").GetComponent<Cat> ();
+	}
+
+	void FixedUpdate () {
+		Vector3 directionToCat = new Vector3 ();
+		directionToCat = cat.position - this.transform.position;
+
+		if (Vector3.Angle (this.transform.forward, directionToCat) <= 130f) {
+			Ray mouseRay = new Ray (this.transform.position, directionToCat);
+			RaycastHit mouseRayHitInfo;
+
+			if (Physics.Raycast (mouseRay, out mouseRayHitInfo, 100f) == true) {
+				if (mouseRayHitInfo.collider.tag == "Cat" && mouseRayHitInfo.distance < 15f) {
+					this.GetComponent<Rigidbody> ().AddForce (-directionToCat.normalized * 10000f);
+				}
+			}
+		}
+	}
+}
